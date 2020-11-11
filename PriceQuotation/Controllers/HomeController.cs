d89651df6cba1c7875714f4db67df19c5a59a9a0
simrangill6054 <59968848+simrangill6054@users.noreply.github.com>
2020-11-11 15:36:27 -0500

    Main Commit

---
+++ PriceQuotation/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotation.Models;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Discount = 0;
+            ViewBag.Total = 0;
+            ViewBag.DiscountPercent = 0;
+            ViewBag.Sale = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Quotation quote)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DiscountPercent = quote.CalcDiscountPercent();
+                ViewBag.Discount = quote.CalculateDiscount();
+                ViewBag.Total = quote.CalculateTotal();
+                ViewBag.Sale = quote.CalcSale();
+            } 
+            else
+            {
+                ViewBag.Discount = 0;
+                ViewBag.Total = 0;
+                ViewBag.DiscountPercent = 0;
+                ViewBag.Sale = 0;
+            }
+            return View(quote);
+        }
+    }
+}
---
+++ PriceQuotation/Models/Quotation.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class Quotation
+    {
+        [Required(ErrorMessage = "Please enter Total Units Used.")]
+        [Range(0, 100000000, ErrorMessage = "Total units used must be greater than zero.")]
+        public double? TotalUnits { get; set; }
+
+        [Required(ErrorMessage = "Please enter Price Per Unit.")]
+        [Range(0, 100000000, ErrorMessage = "Price per unit used must be greater than zero.")]
+        public double? PricePerUnit { get; set; }
+
+
+        public double DiscountPercent,Sale;
+
+        public double CalcSale()
+        {
+            Sale = PricePerUnit.Value * TotalUnits.Value;
+            return Sale;
+        }
+
+        public double CalcDiscountPercent()
+        {
+            Sale = CalcSale();
+            if (TotalUnits.HasValue && PricePerUnit.HasValue)
+            {
+                if (Sale>5000)
+                {
+                    DiscountPercent = 5;
+                    return DiscountPercent;
+                }
+                else
+                {
+                    DiscountPercent = 2;
+                    return DiscountPercent;
+                }
+                
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double CalculateDiscount() {
+            if (TotalUnits.HasValue)
+            {
+                DiscountPercent=CalcDiscountPercent();
+                return Sale * (DiscountPercent / 100);  
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            if (TotalUnits.HasValue && PricePerUnit.HasValue)
+            {
+                double discount = CalculateDiscount();
+                return Sale - discount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
